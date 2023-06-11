98c6cc38234cac9e36cbeb6df0cec0e1cd481a5b
Nathan Mertins <nwmertins@gmail.com>
2023-06-11 07:46:21 -0500

    respond to multiple commands from the same connection

--- src/Server.cs
+++ src/Server.cs
@@ -8,10 +8,19 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
-TcpClient client = server.AcceptTcpClient();
-NetworkStream stream = client.GetStream();
+byte[] bytes = new byte[256];
 
-string response = "+PONG\r\n";
-Byte[] data = System.Text.Encoding.ASCII.GetBytes(response);
+while (true)
+{
+    using TcpClient client = server.AcceptTcpClient();
+    NetworkStream stream = client.GetStream();
 
-stream.Write(data, 0, data.Length);
+    int i;
+
+    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+        string response = "+PONG\r\n";
+        byte[] data = System.Text.Encoding.ASCII.GetBytes(response);
+        stream.Write(data, 0, data.Length);
+    }
+}
